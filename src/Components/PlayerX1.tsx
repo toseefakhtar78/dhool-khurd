7bd953b32d1ba947f6fa6bcb16dbd9ce778bc1e7
waheed <waheed.jutt2323@gmail.com>
2024-08-01 11:20:13 +0500

    commit from Ayaz Goraya

---
+++ src/Components/PlayerX1.tsx
@@ -0,0 +1,10 @@
+
+function PlayerX1() {
+  return (
+    <div className=" text-3xl flex items-center justify-center text-yellow-500 ">
+     <span> Here Our Team info is:</span>
+    </div>
+  )
+}
+
+export default PlayerX1
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,20 @@
+import PlayerX1 from "@/Components/PlayerX1";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-              src="/logo.png"
-              alt="Logo"
-              width={300}
-              height={300}
-              priority
-            />
-             <Image
-              src="/logo.png"
-              alt="Logo"
-              width={300}
-              height={300}
-              priority
-            />
-    </main>
+    <>
+      <div className="flex  flex-col items-center justify-between p-10 ">
+        <div className="text-4xl text-yellow-500 pt-[-100px]">
+          Welcome to official website of Dhool Khurd Cricket Club{" "}
+        </div>
+        <Image src="/logo.png" alt="Logo" width={300} height={100} priority />
+        {/* <span className="text-3xl hover:underline text-yellow-500">
+          DHOOL KHURD
+        </span> */}
+          <PlayerX1/>
+      </div>
+    
+    </>
   );
 }
