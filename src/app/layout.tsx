852ae6351699465be08a222873066f02ce0815d0
waheed <waheed.jutt2323@gmail.com>
2024-08-03 22:25:37 +0500

    "Fixed indentation and added missing closing tags in layout.tsx"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleTagManager } from "@next/third-parties/google";
 import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,17 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-     
-      <head><GoogleTagManager gtmId="G-3WF9PRTKHS" />
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1241486495309147"
-        crossOrigin="anonymous"
-      ></Script>
+      <head>
+        <GoogleTagManager gtmId="G-3WF9PRTKHS" />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1241486495309147"
+          crossOrigin="anonymous"
+        ></Script>
       </head>
       <body className={inter.className}>
         {children}
-        
+
         <footer className="text-yellow-200">
           Developed By: Dr. Ayaz Ahmed Goraya
         </footer>
